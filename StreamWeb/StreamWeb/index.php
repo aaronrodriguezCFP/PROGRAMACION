4d80c5414a4516cfdce0accebc22b932088d519f
aaronrodriguezCFP <aaron.rodriguez@campusfp.es>
2025-01-29 11:41:52 +0100

    Delete StreamWeb/StreamWeb directory

--- StreamWeb/StreamWeb/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: vista/lista_usuarios.php");
-exit();
-?>
--- StreamWeb/StreamWeb/vista/eliminar_usuario.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../controlador/UsuarioController.php';
-
-$controller = new UsuarioController();
-$controller->eliminarUsuario($_GET['id']);
-header("Location: lista_usuarios.php");
-?>
